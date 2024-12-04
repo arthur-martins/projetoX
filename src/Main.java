7b90b323dc2789b386c111005ae21da63eeb81a5
arthur-martins <arthurm.dev@gmail.com>
2024-12-04 07:13:49 -0300

    Adiciona verificação de maioridade.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Obter o nome do cliente
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite seu nome: ");
         String nome = scanner.nextLine();
         System.out.println("Olá " + nome + "!");
+
+        // Obter a idade do cliente
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade!");
+        } else {
+            System.out.println("Você é menor de idade!");
+        }
     }
 }
