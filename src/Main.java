5b29f92c688f593c768f581816e0d319c822312a
arthur-martins <arthurm.dev@gmail.com>
2024-12-04 07:09:17 -0300

    Voltando versão

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Olá " + nome + "!");
+        System.out.println("Hello World!");
     }
 }
