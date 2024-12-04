8db5c024b83e40b58e5165a11bcddb46b3dff447
arthur-martins <arthurm.dev@gmail.com>
2024-12-04 07:05:23 -0300

    Adiciona interação com o usuário.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Olá " + nome + "!");
     }
 }
